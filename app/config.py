0b53de3d639ed5ac11df49e91d84112e364e483d
Emmanuel <endiesworld@gmail.com>
2022-11-25 02:22:28 +0100

    initialized and started fastapi

---
+++ app/config.py
@@ -0,0 +1,30 @@
+import logging
+
+from pydantic import BaseSettings
+"""
+    Here, we defined a Settings class with two attributes:
+
+    environment - defines the environment (i.e., dev, stage, prod)
+    testing - defines whether or not we're in test mode
+    BaseSettings, from pydantic, validates the data so that when we create 
+    an instance of Settings, environment and testing will have types of str and bool,
+    respectively.
+
+    BaseSettings also automatically reads from environment variables for these 
+    config settings. In other words, environment: str = "dev" is equivalent to 
+    os.getenv("ENVIRONMENT", "dev").
+    
+"""
+
+
+log = logging.getLogger("uvicorn")
+
+
+class Settings(BaseSettings):
+    environment: str = "dev"
+    testing: bool = 0
+
+
+def get_settings() -> BaseSettings:
+    log.info("Loading config settings from the environment...")
+    return Settings()
---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Depends, status
+
+from app.config import Settings, get_settings
+
+app = FastAPI()
+
+
+@app.get("/ping", status_code=status.HTTP_200_OK)
+def pong(settings: Settings = Depends(get_settings)):
+    return {
+        "ping": "pong!",
+        "environment": settings.environment,
+        "testing": settings.testing
+        }
